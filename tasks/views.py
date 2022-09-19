ab75f22cd0f6d81c2c498f0cd2f3b279a2608946
noceive <nocivescience@gmail.com>
2022-09-18 23:10:12 -0300

    mi primer commit

---
+++ tasks/views.py
@@ -0,0 +1,54 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+from django.db import IntegrityError
+from django.contrib.auth import login,logout, authenticate
+
+# Create your views here.
+def home(request):
+    return render(request,'home.html')
+def signup(request):
+    if request.method=='GET':
+        return render(request,'signup.html',{
+        'form': UserCreationForm
+        })
+    else:
+        if request.POST['password1']==request.POST['password2']:
+            try:
+                #registrar usuario
+                user=User.objects.create_user(
+                username=request.POST['username'],
+                password=request.POST['password1']
+                )
+                user.save()
+                login(request,user)
+                return redirect('tasks')
+            except IntegrityError:
+                return render(request,'signup.html',{
+                    'form':UserCreationForm,
+                    'error': 'User already exists'
+                })
+        return render(request,'signup.html',{
+            'form': UserCreationForm,
+            'error': 'Passwords doesnt match'
+        })
+def tasks(request):
+    return render(request,'tasks.html')
+def signout(request):
+    logout(request)
+    return redirect('home')
+def signin(request):
+    if request.method=='GET':
+        return render(request,'signin.html',{
+        'form':AuthenticationForm
+        })
+    else:
+        user=authenticate(request,username=request.POST['username'], password=request.POST['password'])
+        if user is None:
+            return render(request,'signin.html',{
+                'form':AuthenticationForm,
+                'error': 'Username or password is incorrect'
+            })
+        else:
+            login(request,user)
+            return redirect('tasks')
