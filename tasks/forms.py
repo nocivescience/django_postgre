5dc90d841c9d99565736e76030f8687288c6fd69
noceive <nocivescience@gmail.com>
2022-09-20 00:53:01 -0300

    terminado

--- tasks/forms.py
+++ tasks/forms.py
@@ -1,6 +1,14 @@
+from logging import PlaceHolder
+from tkinter import Widget
 from django.forms import ModelForm
 from .models import Task
-class TaskForm(ModelForm):
+from django import forms
+class TaskForm(forms.ModelForm):
     class Meta:
         model=Task
         fields=['title', 'description', 'important']
+        widgets={                                   # widgets en minusculas
+            'title':forms.TextInput(attrs={'class': 'form-control my-2','placeholder':'title...'}),
+            'description': forms.Textarea(attrs={'class':'form-control my-2', 'placeholder':'description...'}),
+            'important': forms.CheckboxInput(attrs={'class':'form-check-input my-2'})
+        }
