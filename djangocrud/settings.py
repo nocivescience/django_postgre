5785927d91530cf5eb6841e0a0bf660c174b3407
noceive <nocivescience@gmail.com>
2022-10-07 00:31:13 -0300

    volviendo al estado original

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -85,7 +85,6 @@ DATABASES = {
         default='postgresql://postgres:postgres@localhost/postgres',
         conn_max_age=600
     )
-    
     # 'default': {
     #     'ENGINE': 'django.db.backends.sqlite3',
     #     'NAME': BASE_DIR / 'db.sqlite3',
